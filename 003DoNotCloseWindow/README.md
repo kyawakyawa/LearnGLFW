6b845a07bc179836bb5a1d02f394e53992beccc6
kyawakyawa <ikaairplanef15@yahoo.co.jp>
2019-02-18 01:08:20 +0900

    do not close window

---
+++ 003DoNotCloseWindow/README.md
@@ -0,0 +1,9 @@
+# 003 Do Not Close Window
+## usage
+### Linux or Mac
+```
+$ ./set_cmake_with_clang.sh
+$ cd build
+$ make
+$ ./build/
+```
---
+++ 003DoNotCloseWindow/main.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <GLFW/glfw3.h>
+int main() {
+  if (glfwInit() == GL_FALSE) {
+    printf("failed initializing GLFW.\n");
+    return 1;
+  }
+
+  printf("success initializing GLFW.\n");
+
+  GLFWwindow *const window(glfwCreateWindow(640, 480, "Test Window", NULL, NULL));
+  if (window == NULL) {
+    printf("failed creating GLFW window.");
+    return 1;
+  }
+
+  printf("success openning GLFW.\n");
+
+  // Target window
+  glfwMakeContextCurrent(window);
+  // Select Background Color
+  glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+  // When the window is open
+  while (glfwWindowShouldClose(window) == GL_FALSE) {
+    // Clear Buffer
+    glClear(GL_COLOR_BUFFER_BIT);
+    //
+    // writing process
+    //
+    // exchange color buffer
+    glfwSwapBuffers(window);
+    // Fetch Event
+    glfwWaitEvents();
+  }
+
+  return 0;
+}
