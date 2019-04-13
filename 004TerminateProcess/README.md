1255e67f4b78a48942925c2e315f86e5dc50d54c
kyawakyawa <ikaairplanef15@yahoo.co.jp>
2019-04-13 15:57:08 +0900

    Terminate process

---
+++ 004TerminateProcess/README.md
@@ -0,0 +1,9 @@
+# 004 Terminate Process
+## usage
+### Linux or Mac
+```
+$ ./set_cmake_with_clang.sh
+$ cd build
+$ make
+$ ./build/
+```
---
+++ 004TerminateProcess/main.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <GLFW/glfw3.h>
+int main() {
+  if (glfwInit() == GL_FALSE) {
+    printf("failed initializing GLFW.\n");
+    return 1;
+  }
+
+  printf("success initializing GLFW.\n");
+
+  // register terminating process 
+  atexit(glfwTerminate);
+
+  //Create Window
+  GLFWwindow *const window(glfwCreateWindow(640, 480, "Test Window", NULL, NULL));
+  if (window == NULL) {
+    printf("failed creating GLFW window.");
+    return 1;
+  }
+
+  printf("success openning GLFW.\n");
+
+  // Target window
+  glfwMakeContextCurrent(window);
+  // Select Background Color
+  glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+  // When the window is open
+  while (glfwWindowShouldClose(window) == GL_FALSE) {
+    // Clear Buffer
+    glClear(GL_COLOR_BUFFER_BIT);
+    //
+    // writing process
+    //
+    // exchange color buffer
+    glfwSwapBuffers(window);
+    // Fetch Event
+    glfwWaitEvents();
+  }
+
+  printf("reaching the end of the main function\n");
+
+  return 0;
+}
